0e3bf5fbf80d5cd94be41bb01f16f960fbf9d67b
Hamid Mousavi <hamidabadani63@gmail.com>
2025-05-11 21:31:18 +0330

    cookies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import yt_dlp
+import os
 
 app = Flask(__name__)
 
@@ -9,12 +10,23 @@ def download():
     if not video_url:
         return jsonify({"error": "Missing 'url' parameter"}), 400
 
+    # ذخیره محتوای کوکی از متغیر محیطی
+    cookies_env = os.getenv("COOKIES_DATA")
+    cookies_path = "cookies.txt"
+
+    if cookies_env:
+        with open(cookies_path, "w", encoding="utf-8") as f:
+            f.write(cookies_env)
+
     ydl_opts = {
         'quiet': True,
         'skip_download': True,
         'forcejson': True,
         'extract_flat': False,
     }
+
+    if cookies_env:
+        ydl_opts['cookiefile'] = cookies_path
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -34,7 +46,8 @@ def download():
         return jsonify({"error": str(e)}), 500
 
 # if __name__ == "__main__":
-#     app.run(debug=True)
+#     app.run(host="0.0.0.0", port=5000)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
