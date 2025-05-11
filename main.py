98046588bbd2ceb28e32ffb432e302fe5ad08e70
Hamid Mousavi <hamidabadani63@gmail.com>
2025-05-11 23:35:13 +0330

    fix instagram

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    app.run_polling()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
